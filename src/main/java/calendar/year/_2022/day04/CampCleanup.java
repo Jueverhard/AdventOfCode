b65cfb2b974e7503c3cc7d64a0acf78be06d73b0
Jueverhard <48631987+Jueverhard@users.noreply.github.com>
2023-07-25 14:58:14 +0200

    🎨 Utilisation de regex pour extraire les nombres (remarques MR !13)

--- src/main/java/calendar/year/_2022/day04/CampCleanup.java
+++ src/main/java/calendar/year/_2022/day04/CampCleanup.java
@@ -7,6 +7,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CampCleanup extends Exercise {
 
@@ -14,19 +18,20 @@ public class CampCleanup extends Exercise {
         super(date);
     }
 
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
+
     @Override
     public String run(Part part, boolean testMode) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(this.getInputPath(testMode)))) {
             String line;
             int containedPairs = 0;
             while ((line = br.readLine()) != null) {
-                String[] sections = line.split(",");
-                String[] leftSections = sections[0].split("-");
-                String[] rightSections = sections[1].split("-");
-                int leftStart = Integer.parseInt(leftSections[0]);
-                int leftEnd = Integer.parseInt(leftSections[1]);
-                int rightStart = Integer.parseInt(rightSections[0]);
-                int rightEnd = Integer.parseInt(rightSections[1]);
+                List<Integer> sectionsIdentifiers = extractSectionsIdentifiers(line);
+                int leftStart = sectionsIdentifiers.get(0);
+                int leftEnd = sectionsIdentifiers.get(1);
+                int rightStart = sectionsIdentifiers.get(2);
+
+                int rightEnd = sectionsIdentifiers.get(3);
                 boolean pairsOverlap;
                 if (Part.PART_1.equals(part)) {
                     pairsOverlap = (leftStart <= rightStart && leftEnd >= rightEnd) ||
@@ -42,5 +47,14 @@ public class CampCleanup extends Exercise {
 
             return print(containedPairs);
         }
+    }
+
+    private List<Integer> extractSectionsIdentifiers(String input) {
+        Matcher sectionsMatcher = NUMBER_PATTERN.matcher(input);
+        List<Integer> sectionsIdentifiers = new ArrayList<>();
+        while (sectionsMatcher.find()) {
+            sectionsIdentifiers.add(Integer.parseInt(sectionsMatcher.group()));
+        }
+        return sectionsIdentifiers;
     }
 }
