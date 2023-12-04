1216e13dae06f30b78993340dbaba9778e8f8ae2
Jueverhard <48631987+Jueverhard@users.noreply.github.com>
2023-12-04 18:33:55 +0100

    ✨ 2023 - day 4 : second exercise

--- src/main/java/calendar/year/_2023/day04/Card.java
+++ src/main/java/calendar/year/_2023/day04/Card.java
@@ -2,13 +2,11 @@ package calendar.year._2023.day04;
 
 import java.util.Set;
 
-public record Card(Set<Integer> winningNumbers, Set<Integer> possessedNumbers) {
+public record Card(int id, Set<Integer> winningNumbers, Set<Integer> possessedNumbers) {
 
-    public int computeScore() {
-        long nbWinningCards = winningNumbers.stream()
+    public int computeNbWinningCards() {
+        return (int) winningNumbers.stream()
                 .filter(possessedNumbers::contains)
                 .count();
-
-        return (int) Math.pow(2, nbWinningCards - 1d);
     }
 }
