073b196bd8e1150ccd80cc27fc88ca9cc3ff8e66
Jueverhard <48631987+Jueverhard@users.noreply.github.com>
2023-12-11 11:56:19 +0100

    ✨ 2023 - day 9 : first exercise

---
+++ src/main/java/calendar/year/_2023/day09/Sequence.java
@@ -0,0 +1,37 @@
+package calendar.year._2023.day09;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public record Sequence(List<Integer> numbers) {
+
+    public int computeNextValue() {
+        List<Sequence> subSequences = computeSubSequences();
+
+        return subSequences.stream()
+                .mapToInt(Sequence::getLastValue)
+                .sum();
+    }
+
+    private int getLastValue() {
+        return numbers.get(numbers.size() - 1);
+    }
+
+    private List<Sequence> computeSubSequences() {
+        List<Sequence> subSequences = new ArrayList<>();
+        Sequence differencesSubSequence = new Sequence(numbers);
+        subSequences.add(differencesSubSequence);
+
+        while (differencesSubSequence.numbers().stream().anyMatch(value -> 0 != value)) {
+            Sequence finalDifferencesSubSequence = differencesSubSequence;
+            List<Integer> differences = IntStream.range(1, differencesSubSequence.numbers().size())
+                    .mapToObj(i -> finalDifferencesSubSequence.numbers().get(i) - finalDifferencesSubSequence.numbers().get(i - 1))
+                    .toList();
+            differencesSubSequence = new Sequence(differences);
+            subSequences.add(differencesSubSequence);
+        }
+        return subSequences;
+    }
+}
---
+++ src/test/java/calendar/year/_2023/day09/MirageMaintenanceTest.java
@@ -0,0 +1,35 @@
+package calendar.year._2023.day09;
+
+import calendar.BaseTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import utils.enums.Part;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+class MirageMaintenanceTest extends BaseTest {
+
+    private final MirageMaintenance exercise = initializeExercise(MirageMaintenance.class);
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(Part.PART_1, false, 2075724761),
+                Arguments.of(Part.PART_1, true, 114)//,
+//                Arguments.of(Part.PART_2, false, 0),
+//                Arguments.of(Part.PART_2, true, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void run_test(Part part, boolean testMode, Object expectedResult) throws IOException {
+        // ARRANGE
+        // ACT
+        String res = exercise.run(part, testMode);
+
+        // ASSERT
+        assertEquals(expectedResult, res);
+    }
+}
