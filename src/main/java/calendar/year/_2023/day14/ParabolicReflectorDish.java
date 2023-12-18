29480b9be769139ad74689cf3a2eab6eaa03cdc1
Jueverhard <48631987+Jueverhard@users.noreply.github.com>
2023-12-18 12:47:32 +0100

    ✨ Pattern detection

--- src/main/java/calendar/year/_2023/day14/ParabolicReflectorDish.java
+++ src/main/java/calendar/year/_2023/day14/ParabolicReflectorDish.java
@@ -8,7 +8,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ParabolicReflectorDish extends Exercise {
 
@@ -41,14 +43,22 @@ public class ParabolicReflectorDish extends Exercise {
         if (Part.PART_1 == part) {
             dish.rollNorth();
         } else {
+            Map<Integer, Integer> nbCyclesPerDish = new HashMap<>();
             int nbIterations = 0;
-            // TODO : trick to find to NOT do all iterations (pattern ? maths ? ...?)
             while (nbIterations < 1000000000) {
                 dish.rollNorth();
                 dish.rollWest();
                 dish.rollSouth();
                 dish.rollEast();
                 nbIterations++;
+
+                if (!nbCyclesPerDish.containsKey(dish.hashCode())) {
+                    nbCyclesPerDish.put(dish.hashCode(), nbIterations);
+                } else {
+                    int cycleLength = nbIterations - nbCyclesPerDish.get(dish.hashCode());
+                    int nbCyclesLeftToDo = (1000000000 - nbIterations) % cycleLength;
+                    nbIterations = 1000000000 - nbCyclesLeftToDo;
+                }
             }
         }
 
