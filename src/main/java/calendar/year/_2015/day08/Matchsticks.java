7afe23abd93f17437d1c89b1839cac559868e939
Jueverhard <48631987+Jueverhard@users.noreply.github.com>
2024-05-21 14:47:07 +0200

    🎨 Simplification

--- src/main/java/calendar/year/_2015/day08/Matchsticks.java
+++ src/main/java/calendar/year/_2015/day08/Matchsticks.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.regex.Pattern;
 
 public class Matchsticks extends Exercise {
@@ -18,50 +17,39 @@ public class Matchsticks extends Exercise {
 
     private static final Pattern ENCLOSING_QUOTES = Pattern.compile("^\"|\"$");
     private static final Pattern ESCAPED_SINGLE_CHAR = Pattern.compile("\\\\[\\\"\\\\]");
-    private static final Pattern ESCAPED_HEXA_CODE = Pattern.compile("\\\\x[\\dabcdef]{2}");
+    private static final Pattern ESCAPED_HEXA_CODE = Pattern.compile("\\\\x[\\da-f]{2}");
 
     @Override
     public String run(Part part, boolean testMode) throws IOException {
         List<String> inputLines = Parser.parseLines(getInputPath(testMode));
 
-        Function<String, Integer> deltaComputationFunction = Part.PART_1 == part ?
-                this::computeNbOfDiscountedCharacters :
-                this::computeNbOfNewlyNecessariesCharacters;
+        // Prepare the delta caused in string size for each pattern
+        Map<Pattern, Integer> deltaStringSize = Part.PART_1 == part ?
+                Map.of(
+                        ENCLOSING_QUOTES, 1,
+                        ESCAPED_SINGLE_CHAR, 1,
+                        ESCAPED_HEXA_CODE, 3
+                ) :
+                Map.of(
+                        ENCLOSING_QUOTES, 2,
+                        ESCAPED_SINGLE_CHAR, 2,
+                        ESCAPED_HEXA_CODE, 1
+                );
+
         int toBeDiscountedChars = inputLines.stream()
-                .map(deltaComputationFunction)
+                .map(input -> computeDeltaInStringSize(input, deltaStringSize))
                 .reduce(0, Integer::sum);
 
         return print(toBeDiscountedChars);
     }
 
     /**
-     * @param input The string input to analyze
-     * @return the number of characters that is lost during string interpolation
+     * @param input                 The string input to analyze
+     * @param deltaCausedPerPattern The delta caused in string size per pattern
+     * @return the number of characters that is changing in the string after computation
      */
-    private int computeNbOfDiscountedCharacters(String input) {
-        Map<Pattern, Integer> nbDiscountedCharPerPattern = Map.of(
-                ENCLOSING_QUOTES, 1,
-                ESCAPED_SINGLE_CHAR, 1,
-                ESCAPED_HEXA_CODE, 3
-        );
-
-        return nbDiscountedCharPerPattern.entrySet().stream()
-                .map(entry -> entry.getValue() * Math.toIntExact(entry.getKey().matcher(input).results().count()))
-                .reduce(0, Integer::sum);
-    }
-
-    /**
-     * @param input The string input to analyze
-     * @return the number of new characters that is needed for string interpolation
-     */
-    private int computeNbOfNewlyNecessariesCharacters(String input) {
-        Map<Pattern, Integer> nbDiscountedCharPerPattern = Map.of(
-                ENCLOSING_QUOTES, 2,
-                ESCAPED_SINGLE_CHAR, 2,
-                ESCAPED_HEXA_CODE, 1
-        );
-
-        return nbDiscountedCharPerPattern.entrySet().stream()
+    private int computeDeltaInStringSize(String input, Map<Pattern, Integer> deltaCausedPerPattern) {
+        return deltaCausedPerPattern.entrySet().stream()
                 .map(entry -> entry.getValue() * Math.toIntExact(entry.getKey().matcher(input).results().count()))
                 .reduce(0, Integer::sum);
     }
