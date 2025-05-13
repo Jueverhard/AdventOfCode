479372bb137a3edf7af5e3d999528a2d2ed36197
Jueverhard <48631987+Jueverhard@users.noreply.github.com>
2025-05-13 15:28:14 +0200

    ✨ 2020 - day 9 : second exercise

--- src/main/java/calendar/year/_2020/day09/EncodingError.java
+++ src/main/java/calendar/year/_2020/day09/EncodingError.java
@@ -6,6 +6,7 @@ import utils.fileparser.Parser;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -25,15 +26,32 @@ public class EncodingError extends Exercise {
                 .toList();
         int range = testMode ? 5 : 25;
 
-        long result = IntStream.range(range, numbers.size())
+        // Invalid number computation
+        long invalidNumber = IntStream.range(range, numbers.size())
                 .filter(i -> !isSumIncluded(numbers.get(i), numbers.subList(i - range, i)))
                 .mapToLong(numbers::get)
                 .findFirst()
                 .orElseThrow();
 
+        long result;
+        if (Part.PART_1 == part) {
+            result = invalidNumber;
+        } else {
+            List<Long> contiguousNumbers = findContiguousNumbersSummingTo(
+                    numbers.stream().filter(number -> invalidNumber != number).toList(),
+                    invalidNumber
+            );
+            result = Collections.min(contiguousNumbers) + Collections.max(contiguousNumbers);
+        }
+
         return print(result);
     }
 
+    /**
+     * @param expected The expected sum.
+     * @param numbers  The summable numbers.
+     * @return Whether a two numbers sum matches the expected value.
+     */
     private boolean isSumIncluded(long expected, List<Long> numbers) {
         return IntStream.range(0, numbers.size())
                 .mapToObj(i -> IntStream.range(0, numbers.size())
@@ -44,5 +62,20 @@ public class EncodingError extends Exercise {
                 )
                 .flatMap(Set::stream)
                 .anyMatch(sum -> sum == expected);
+    }
+
+    /**
+     * @param numbers  The summable numbers.
+     * @param expected The expected sum.
+     * @return The contiguous set of numbers, if any, that is summable to the expected value.
+     */
+    private List<Long> findContiguousNumbersSummingTo(List<Long> numbers, long expected) {
+        return IntStream.range(0, numbers.size() - 2)
+                .mapToObj(start -> IntStream.range(start + 2, numbers.size())
+                        .mapToObj(end -> numbers.subList(start, end))
+                        .toList()
+                ).flatMap(List::stream)
+                .filter(contiguousNumbers -> expected == contiguousNumbers.stream().reduce(Long::sum).orElseThrow())
+                .findFirst().orElseThrow();
     }
 }
--- src/test/java/calendar/year/_2020/day09/EncodingErrorTest.java
+++ src/test/java/calendar/year/_2020/day09/EncodingErrorTest.java
@@ -16,9 +16,9 @@ class EncodingErrorTest extends BaseTest {
     private static Stream<Arguments> parameters() {
         return Stream.of(
                 Arguments.of(Part.PART_1, false, 31161678),
-                Arguments.of(Part.PART_1, true, 127)//,
-//                Arguments.of(Part.PART_2, false, 0),
-//                Arguments.of(Part.PART_2, true, 0)
+                Arguments.of(Part.PART_1, true, 127),
+                Arguments.of(Part.PART_2, false, 5453868),
+                Arguments.of(Part.PART_2, true, 62)
         );
     }
 
